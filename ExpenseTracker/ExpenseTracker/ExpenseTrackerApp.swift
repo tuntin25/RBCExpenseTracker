e7b614a2f05fb133341fd683bc8dd908f2c63538
Nam Luong <50762424+tuntin25@users.noreply.github.com>
2023-08-22 07:14:32 +0700

    Add files via upload

---
+++ ExpenseTracker/ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Nam Luong on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTracker/ExpenseTracker/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Nam Luong on 22/08/2023.
+//
+
+import Foundation
+import SwiftUI
+extension Color{
+    static let background = Color("Background")
+    static let icon = Color ("Icon")
+    static let text = Color ("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
