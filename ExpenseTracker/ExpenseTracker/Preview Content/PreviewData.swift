8608753c9a8306614a62897e0e79f2159b39ba50
Nam Luong <namluongminh2509@gmail.com>
2023-08-22 21:30:09 +0700

    Add Expense UI

---
+++ ExpenseTracker/ExpenseTracker/Preview Content/PreviewData.swift
@@ -0,0 +1,14 @@
+//
+//  PreviewData.swift
+//  ExpenseTracker
+//
+//  Created by Nam Luong on 22/08/2023.
+//
+
+
+import Foundation
+import SwiftUI
+
+var transactionPreviewData = Transaction(id: 1, date: "01/24/2022", institution: "Desjardin",account: "Visa Desjardins", merchant: "Apple", amount:11.49, type: "debit", categoryId: 801, category: "Software", isPending: false, isTransfer: false, isExpense: true, isEdited: false)
+
+var transactionListPreviewData = [Transaction](repeating: transactionPreviewData, count:10)
