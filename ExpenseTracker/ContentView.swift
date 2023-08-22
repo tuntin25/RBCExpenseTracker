b9bd50a86137fae213f44617d960b37bd874d055
Nam Luong <50762424+tuntin25@users.noreply.github.com>
2023-08-22 07:07:22 +0700

    Add files via upload

---
+++ ExpenseTracker/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  ExpenseTracker
+//
+//  Created by Nam Luong on 22/08/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            ScrollView{
+                VStack(alignment: .leading, spacing: 24){
+                    Text("Overview")
+                        .font(.title2)
+                        .bold()
+                    
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
+            }
+            .background(Color.background)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem{
+                    Image(systemName: "bell.badge")
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(Color.icon,.primary)
+                }
+            }
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group{
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
